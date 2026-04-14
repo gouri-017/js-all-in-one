ba191cc7148d8448593a1c61f36bf796d20839a2
Gouri <gouri7136@gmail.com>
2026-04-14 19:42:54 +0530

    add restart functionality and checks

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 console.log('welcome');
-
-let startButton = document.querySelector('#heading-title')
 let gameOn = false
+let chances = 10;
+let startButton = document.querySelector('#heading-title')
 let headingTitle = document.getElementById('heading-title')
 let showUserGuess = document.getElementById('showUserGuess')
-  const btn = document.querySelector("#numberGuessingGameBtn")
-  const inputValueElement = document.querySelector("#userGuessNumber")
-  let displayMessagePosition = document.getElementById('showMessage')
-  let displayRemainingGuesses = document.getElementById('showRemainingGuesses')
-  const guessesByUser = [];
-  let chances = 10;
-  let actualNumber = Math.floor((Math.random() * 100)+1);
+const btn = document.querySelector("#numberGuessingGameBtn")
+const inputValueElement = document.querySelector("#userGuessNumber")
+let displayMessagePosition = document.getElementById('showMessage')
+let displayRemainingGuesses = document.getElementById('showRemainingGuesses')
+let guessesByUser = [];
+let actualNumber;
+
+function startGame(){
+  gameOn = true;
+  chances = 10;
+  guessesByUser =[]
+  displayRemainingGuesses.innerText = `${chances}`
+  displayMessagePosition.innerText = `  `
+  showUserGuess.innerText = `  `
+  inputValueElement.value ='';
+  actualNumber = Math.floor((Math.random() * 100)+1);
   console.log(actualNumber);
+  headingTitle.innerText = "Game ON!!"
+}
+
+
+
+
+
+
+
 
 
 function numberGuessingLogic(userGuess){
-  let message;
+  let message = 'please enter a number between 1-100';
   userGuess = Number(userGuess)
+  if (Number.isNaN(userGuess) || userGuess>100 || userGuess<1 || userGuess==='')
+  {
+    showUserGuess.innerText = `  `
+    return message;}
+    
   chances --;
    if(userGuess === actualNumber){
      console.log(`Wohoo! you guessed it right! in your ${10-chances +1} attempt`);
@@ -42,8 +65,8 @@ function numberGuessingLogic(userGuess){
 
 startButton.addEventListener('click',()=>{
   
-  gameOn=true;
-  headingTitle.innerText = "Game ON!!"
+  startGame();
+
 })
 
 btn.addEventListener('click',()=>{
@@ -59,7 +82,7 @@ btn.addEventListener('click',()=>{
     displayRemainingGuesses.textContent = `${chances}`
     inputValueElement.value ='';
     if(chances===0){
-    headingTitle.innerText = 'uh,oh! You lost. Wanna give up? or just Play again!'+ headingTitle.innerText;
+    headingTitle.innerText = 'uh,oh! You lost. Wanna give up? or just Play again!';
   console.log('uh,oh! You lost. Wanna give up? or just Play again!');
   gameOn = false;
 }
